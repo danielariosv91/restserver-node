4ea9ab458c2ddb0dd07c8e9d764310bf6bce4115
Daniela Rios <daniela.rios.v91@gmail.com>
2023-09-06 12:20:06 -0300

    create middleware

--- models/server.js
+++ models/server.js
@@ -4,7 +4,16 @@ class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
+
+        this.middlewares();
+
+        /** App Routes */
         this.routes();
+    }
+
+    middlewares = () => {
+        /** Public Directory */
+        this.app.use(express.static('public'));
     }
 
     routes = () => {
